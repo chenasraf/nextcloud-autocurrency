20d66b3650f53701a9a9ec54ac9cf15961592ced
Chen Asraf <casraf@pm.me>
2024-12-01 04:12:24 +0200

    feat: settings page logic

--- lib/Service/FetchCurrenciesService.php
+++ lib/Service/FetchCurrenciesService.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 // SPDX-FileCopyrightText: Chen Asraf <contact@casraf.dev>
 // SPDX-License-Identifier: AGPL-3.0-or-later
@@ -7,92 +8,103 @@ namespace OCA\AutoCurrency\Service;
 
 use Exception;
 
-use OCA\AutoCurrency\Service\CurrencyNotFound;
 use OCA\AutoCurrency\Db\CospendProjectMapper;
-use OCP\AppFramework\Db\DoesNotExistException;
-use OCP\AppFramework\Db\MultipleObjectsReturnedException;
-
 use OCA\AutoCurrency\Db\Currency;
 use OCA\AutoCurrency\Db\CurrencyMapper;
-use OCP\ILogger;
+
+use OCP\AppFramework\Db\DoesNotExistException;
+use OCP\AppFramework\Db\MultipleObjectsReturnedException;
+use OCP\IConfig;
+use Psr\Log\LoggerInterface;
 
 class FetchCurrenciesService {
-  private static $EXCHANGE_URL = 'https://api.exchangerate.host/latest?base={base}';
-  private CurrencyMapper $currencyMapper;
-  private CospendProjectMapper $projectMapper;
-  private ILogger $logger;
+	private static $EXCHANGE_URL = 'https://api.exchangerate.host/latest?base={base}';
+	private IConfig $config;
+	private CurrencyMapper $currencyMapper;
+	private CospendProjectMapper $projectMapper;
+	private LoggerInterface $logger;
 
-  public function __construct(CurrencyMapper $currencyMapper, CospendProjectMapper $projectMapper, ILogger $logger) {
-    $this->currencyMapper = $currencyMapper;
-    $this->projectMapper = $projectMapper;
-    $this->logger = $logger;
-  }
+	public function __construct(
+		IConfig $config,
+		CurrencyMapper $currencyMapper,
+		CospendProjectMapper $projectMapper,
+		LoggerInterface $logger,
+	) {
+		$this->config = $config;
+		$this->currencyMapper = $currencyMapper;
+		$this->projectMapper = $projectMapper;
+		$this->logger = $logger;
+	}
 
-  public function doCron(): void {
-    $projects = $this->projectMapper->findAll();
-    $currencyMap = [];
+	public function doCron(): void {
+		$this->logger->info('Starting cron job to fetch currencies');
+		$projects = $this->projectMapper->findAll();
+		$currencyMap = [];
 
-    foreach ($projects as $project) {
-      $base = $this->getCurrencyName($project->getCurrencyname());
+		foreach ($projects as $project) {
+			$base = $this->getCurrencyName($project->getCurrencyname());
 
-      if (isset($currencyMap[$base])) {
-        $json = $currencyMap[$base];
-      } else {
-        // request currency exchange rates from the API
-        $this->logger->info('Fetching exchange rates for base currency ' . $base);
-        $fp = fopen(str_replace('{base}', $base, FetchCurrenciesService::$EXCHANGE_URL), 'r');
-        $data = stream_get_contents($fp);
-        fclose($fp);
-        $json = json_decode($data, true);
-        $this->logger->info('Fetched exchange rates for base currency: ' . json_encode($json));
-        if ($json['success'] == false) {
-          $this->logger->error(new \Error('Failed to fetch exchange rates for base currency ' . $base));
-          continue;
-        }
-        $currencyMap[$base] = $json;
-      }
+			if (isset($currencyMap[$base])) {
+				$json = $currencyMap[$base];
+			} else {
+				// request currency exchange rates from the API
+				$this->logger->info('Fetching exchange rates for base currency ' . $base);
+				$fp = fopen(str_replace('{base}', $base, FetchCurrenciesService::$EXCHANGE_URL), 'r');
+				$data = stream_get_contents($fp);
+				fclose($fp);
+				$json = json_decode($data, true);
+				$this->logger->info('Fetched exchange rates for base currency: ' . json_encode($json));
+				if ($json['success'] == false) {
+					$this->logger->error(new \Error('Failed to fetch exchange rates for base currency ' . $base));
+					continue;
+				}
+				$currencyMap[$base] = $json;
+			}
 
-      $currencies = $this->findAll($project->id);
+			$currencies = $this->findAll($project->id);
 
-      foreach ($currencies as $currency) {
-        $cur = $this->getCurrencyName($currency->getName());
-        $newRate = floatval(number_format(1 / $json['rates'][$cur], 2));
-        $currency->setExchangeRate($newRate);
-        $this->logger->info('Setting exchange rate for currency ' . $cur . ' to ' . $newRate);
-        $this->currencyMapper->update($currency);
-      }
-    }
-  }
+			foreach ($currencies as $currency) {
+				$cur = $this->getCurrencyName($currency->getName());
+				$newRate = floatval(number_format(1 / $json['rates'][$cur], 2));
+				$currency->setExchangeRate($newRate);
+				$this->logger->info('Setting exchange rate for currency ' . $cur . ' to ' . $newRate);
+				$this->currencyMapper->update($currency);
+			}
+		}
 
-  private function getCurrencyName(string $name): string {
-    // find 3-letter currency code for the base currency
-    preg_match('/([A-Z]{3})/', $name, $matches);
+		$lastUpdate = date('c');
+		$this->config->setAppValue('autocurrency', 'last_update', $lastUpdate);
+	}
 
-    $this->logger->info('Matches: ' . json_encode($matches));
+	private function getCurrencyName(string $name): string {
+		// find 3-letter currency code for the base currency
+		preg_match('/([A-Z]{3})/', $name, $matches);
 
-    if (count($matches) === 2) {
-      $name = $matches[1];
-    }
+		$this->logger->info('Matches: ' . json_encode($matches));
 
-    return $name;
-  }
+		if (count($matches) === 2) {
+			$name = $matches[1];
+		}
 
-  /**
-   * @return list<Currency>
-   */
-  public function findAll(string $projectId): array {
-    return $this->currencyMapper->findAll($projectId);
-  }
+		return $name;
+	}
 
-  /**
-   * @return never
-   */
-  private function handleException(Exception $e) {
-    if ($e instanceof DoesNotExistException ||
-      $e instanceof MultipleObjectsReturnedException) {
-      throw new CurrencyNotFound($e->getMessage());
-    } else {
-      throw $e;
-    }
-  }
+	/**
+	 * @return list<Currency>
+	 */
+	public function findAll(string $projectId): array {
+		return $this->currencyMapper->findAll($projectId);
+	}
+
+	/**
+	 * @return never
+	 */
+	private function handleException(Exception $e) {
+		if ($e instanceof DoesNotExistException ||
+		  $e instanceof MultipleObjectsReturnedException) {
+			throw new CurrencyNotFound($e->getMessage());
+		} else {
+			throw $e;
+		}
+	}
 }
--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,24 @@
     <h2>Auto Currency for Cospend</h2>
     <NcAppSettingsSection name="Cron Settings">
       <section>
-        <form @submit.prevent>
-          <NcSelect
-            v-model="interval"
-            :options="intervals"
-            input-label="Currency conversion rate update interval"
-            required
-            :disabled="interval == null"
-          />
+        <form @submit.prevent @submit="save">
+          <div class="cron-flex">
+            <NcSelect
+              v-model="interval"
+              :options="intervals"
+              input-label="Currency conversion rate update interval"
+              required
+              :disabled="loading"
+            />
+
+            <div class="cron-last-update-container">
+              <NcButton @click="doCron" :disabled="loading">Fetch Rates Now</NcButton>
+
+              <div>
+                Rates last fetched: {{ loading ? "..." : lastUpdateStr }}
+              </div>
+            </div>
+          </div>
           <div class="submit-buttons">
             <NcButton native-type="submit">Save</NcButton>
           </div>
@@ -21,23 +31,25 @@
 </template>
 
 <script>
-// import NcAppContent from '@nextcloud/vue/dist/Components/NcAppContent.js'
 import NcAppSettingsSection from '@nextcloud/vue/dist/Components/NcAppSettingsSection.js'
 import NcSelect from '@nextcloud/vue/dist/Components/NcSelect.js'
 import NcButton from '@nextcloud/vue/dist/Components/NcButton.js'
 import axios from '@nextcloud/axios'
+import { parseISO as parseDate } from 'date-fns/parseISO'
+import { format as formatDate } from 'date-fns/format'
 
 export default {
 	name: 'App',
 	components: {
-		// NcAppContent,
     NcAppSettingsSection,
     NcSelect,
     NcButton,
 	},
   data() {
     return {
+      loading: true,
       interval: null,
+      lastUpdate: null,
       intervalOptions: [
         { label: 'Every hour', value: 1 },
         { label: 'Every 3 hours', value: 3 },
@@ -55,9 +67,12 @@ export default {
   methods: {
     async fetchSettings() {
       try {
+        this.loading = true
         const resp = await axios.get('/cron')
         const data = resp.data.ocs.data
-        console.debug('[DEBUG] Settings fetched', data)
+        this.loading = false
+        console.debug('[DEBUG] Auto Currency settings fetched', data)
+
         const interval = this.getIntervalByValue(data.interval)
         if (interval) {
           console.debug('[DEBUG] Interval found', interval)
@@ -65,8 +80,13 @@ export default {
         } else {
           console.warn('Invalid interval value', data.interval)
         }
+
+        if (data.last_update) {
+          const lastUpdate = parseDate(data.last_update, new Date())
+          this.lastUpdate = lastUpdate
+        }
       } catch (e) {
-        console.error('Failed to fetch settings', e)
+        console.error('Failed to fetch Auto Currency settings', e)
       }
     },
     getIntervalByValue(value) {
@@ -75,10 +95,39 @@ export default {
     getIntervalByLabel(label) {
       return this.intervalOptions.find((x) => x.label === label)
     },
+    async doCron() {
+      try {
+        const resp = await axios.post('/cron/run')
+        const data = resp.data.ocs.data
+        console.debug('[DEBUG] Cron executed', data)
+        this.fetchSettings()
+      } catch (e) {
+        console.error('Failed to run cron', e)
+      }
+    },
+    async save() {
+      try {
+        this.loading = true
+        const interval = this.getIntervalByLabel(this.interval)?.value ?? 24
+        const resp = await axios.put('/cron', { data: { interval } })
+        const data = resp.data.ocs.data
+        this.loading = false
+        console.debug('[DEBUG] Auto Currency settings saved', data)
+        this.fetchSettings()
+      } catch (e) {
+        console.error('Failed to update Auto Currency settings', e)
+      }
+    },
   },
   computed: {
     intervals() {
       return this.intervalOptions.map((x) => x.label)
+    },
+    lastUpdateStr() {
+      if (!this.lastUpdate) {
+      return 'Never'
+      }
+      return formatDate(this.lastUpdate, 'yyyy-MM-dd HH:mm:ss')
     },
     // selectedIntervals() {
     //   return this.intervals.filter(interval => interval.selected)
@@ -96,6 +145,18 @@ export default {
 
   .submit-buttons {
     margin-top: 16px;
+  }
+
+  .cron-flex {
+    display: flex;
+    align-items: start;
+    gap: 16px;
+  }
+
+  .cron-last-update-container {
+    display: flex;
+    flex-direction: column;
+    gap: 8px;
   }
 }
 </style>
