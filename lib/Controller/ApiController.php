a2cc1a12e53da173d1e21af3d8760d6e227f1879
Chen Asraf <casraf@pm.me>
2024-12-01 04:12:24 +0200

    feat: settings page logic

--- lib/Controller/ApiController.php
+++ lib/Controller/ApiController.php
@@ -4,16 +4,62 @@ declare(strict_types=1);
 
 namespace OCA\AutoCurrency\Controller;
 
+use OCA\AutoCurrency\Service\FetchCurrenciesService;
 use OCP\AppFramework\Http;
 use OCP\AppFramework\Http\Attribute\ApiRoute;
 use OCP\AppFramework\Http\Attribute\NoAdminRequired;
 use OCP\AppFramework\Http\DataResponse;
 use OCP\AppFramework\OCSController;
+use OCP\IConfig;
+use OCP\IDateTimeFormatter;
+use OCP\IL10N;
+use OCP\IRequest;
 
 /**
  * @psalm-suppress UnusedClass
  */
 class ApiController extends OCSController {
+	/** @var IConfig */
+	private $config;
+
+	/** @var IL10N */
+	private $l;
+
+	/** @var FetchCurrenciesService */
+	private $service;
+
+	/** @var IDateTimeFormatter */
+	// private $dateTimeFormatter;
+
+	/** @var IJobList */
+	// private $jobList;
+
+	/**
+	 * Admin constructor.
+	 *
+	 * @param Collector $collector
+	 * @param IConfig $config
+	 * @param IL10N $l
+	 * @param IDateTimeFormatter $dateTimeFormatter
+	 * @param IJobList $jobList
+	 */
+	public function __construct(
+		string $appName,
+		IRequest $request,
+		IConfig $config,
+		IL10N $l,
+		FetchCurrenciesService $service,
+		// IDateTimeFormatter $dateTimeFormatter,
+		// IJobList $jobList,
+	) {
+		parent::__construct($appName, $request);
+		$this->config = $config;
+		$this->l = $l;
+		$this->service = $service;
+		// $this->dateTimeFormatter = $dateTimeFormatter;
+		// $this->jobList = $jobList;
+	}
+
 	/**
 	 * An example API endpoint
 	 *
@@ -36,11 +82,55 @@ class ApiController extends OCSController {
 	 *
 	 * 200: Data returned
 	 */
-	#[NoAdminRequired]
+	// #[NoAdminRequired]
 	#[ApiRoute(verb: 'GET', url: '/api/cron')]
 	public function getCronInfo(): DataResponse {
+		$lastUpdate = $this->config->getAppValue('autocurrency', 'last_update', '');
+		if ($lastUpdate === '') {
+			$lastUpdate = null;
+		}
+
+		$interval = intval($this->config->getAppValue('autocurrency', 'cron_interval', '24'));
+		// if ($lastUpdate !== null) {
+		//   $lastUpdate = $this->dateTimeFormatter->formatDate($lastUpdate)
+		// }
+
 		return new DataResponse(
-			['last_update' => '2021-09-01 00:00:00', 'interval' => 24]
+			['last_update' => $lastUpdate, 'interval' => $interval]
+		);
+	}
+
+	/**
+	 * Run cron immediately.
+	 *
+	 * @return DataResponse<Http::STATUS_OK, array{status:string}, array{}>
+	 *
+	 * 200: Data returned
+	 */
+	// #[NoAdminRequired]
+	#[ApiRoute(verb: 'POST', url: '/api/cron/run')]
+	public function runCron(): DataResponse {
+		$this->service->doCron();
+
+		return new DataResponse(
+			['status' => 'OK']
+		);
+	}
+
+	/**
+	 * Update auto currency settings.
+	 *
+	 * @return DataResponse<Http::STATUS_OK, array{status:string}, array{}>
+	 *
+	 * 200: Data returned
+	 */
+	// #[NoAdminRequired]
+	#[ApiRoute(verb: 'PUT', url: '/api/cron')]
+	public function updateSettings(): DataResponse {
+		$interval = $this->request->getParam('data')['interval'];
+		$this->config->setAppValue('autocurrency', 'cron_interval', "$interval");
+		return new DataResponse(
+			['status' => 'OK']
 		);
 	}
 }
--- lib/Cron/FetchCurrenciesJob.php
+++ lib/Cron/FetchCurrenciesJob.php
@@ -1,27 +1,29 @@
 <?php
+
 namespace OCA\AutoCurrency\Cron;
 
 use OCA\AutoCurrency\Service\FetchCurrenciesService;
-use OCP\BackgroundJob\TimedJob;
 use OCP\AppFramework\Utility\ITimeFactory;
-use OCP\ILogger;
+use OCP\BackgroundJob\TimedJob;
+use Psr\Log\LoggerInterface;
 
 class FetchCurrenciesJob extends TimedJob {
-  private FetchCurrenciesService $service;
-  private ILogger $logger;
+	private FetchCurrenciesService $service;
+	private LoggerInterface $logger;
 
-  public function __construct(ITimeFactory $time, FetchCurrenciesService $service, ILogger $logger) {
-    parent::__construct($time);
-    $this->service = $service;
-    $this->logger = $logger;
+	public function __construct(ITimeFactory $time, FetchCurrenciesService $service, LoggerInterface $logger) {
+		parent::__construct($time);
+		$this->service = $service;
+		$this->logger = $logger;
 
-    // Run once a day
-    $this->setInterval(3600 * 24);
-    $this->setTimeSensitivity(\OCP\BackgroundJob\IJob::TIME_INSENSITIVE);
-  }
+		// Run once a day
+		$interval = intval($this->config->getAppValue('autocurrency', 'cron_interval', '24'));
+		$this->setInterval(3600 * $interval);
+		$this->setTimeSensitivity(\OCP\BackgroundJob\IJob::TIME_INSENSITIVE);
+	}
 
-  protected function run($arguments) {
-    $this->logger->info('Running cron job for FetchCurrenciesTask - args: ' . json_encode($arguments));
-    $this->service->doCron();
-  }
+	protected function run($arguments): void {
+		$this->logger->info('Running cron job for FetchCurrenciesTask - args: ' . json_encode($arguments));
+		$this->service->doCron();
+	}
 }
