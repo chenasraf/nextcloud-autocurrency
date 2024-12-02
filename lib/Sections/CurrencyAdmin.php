a8e3347b158d8184c7e00374ed37e1f5409d52e3
Chen Asraf <casraf@pm.me>
2024-12-02 14:08:18 +0200

    chore: fix lints

--- lib/Sections/CurrencyAdmin.php
+++ lib/Sections/CurrencyAdmin.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace OCA\AutoCurrency\Sections;
 
 use OCP\IL10N;
@@ -6,27 +7,27 @@ use OCP\IURLGenerator;
 use OCP\Settings\IIconSection;
 
 class CurrencyAdmin implements IIconSection {
-    private IL10N $l;
-    private IURLGenerator $urlGenerator;
+	private IL10N $l;
+	private IURLGenerator $urlGenerator;
 
-    public function __construct(IL10N $l, IURLGenerator $urlGenerator) {
-        $this->l = $l;
-        $this->urlGenerator = $urlGenerator;
-    }
+	public function __construct(IL10N $l, IURLGenerator $urlGenerator) {
+		$this->l = $l;
+		$this->urlGenerator = $urlGenerator;
+	}
 
-    public function getIcon(): string {
-        return $this->urlGenerator->imagePath('autocurrency', 'app.svg');
-    }
+	public function getIcon(): string {
+		return $this->urlGenerator->imagePath('autocurrency', 'app.svg');
+	}
 
-    public function getID(): string {
-        return 'autocurrency';
-    }
+	public function getID(): string {
+		return 'autocurrency';
+	}
 
-    public function getName(): string {
-        return $this->l->t('Auto Currency');
-    }
+	public function getName(): string {
+		return $this->l->t('Auto Currency');
+	}
 
-    public function getPriority(): int {
-        return 98;
-    }
+	public function getPriority(): int {
+		return 98;
+	}
 }
