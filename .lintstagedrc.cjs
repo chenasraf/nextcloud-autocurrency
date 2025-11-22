a97d45d6a9ca39a6421f4d7d65af96929bec2a3c
Chen Asraf <casraf@pm.me>
2025-11-23 00:55:45 +0200

    chore: exclude gen/ changes from php test trigger

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -2,9 +2,16 @@ module.exports = {
   '*.{ts,vue}': ['eslint --fix'],
   '*.{scss,vue,ts,md}': ['prettier --write'],
   '*.json': (files) => {
-    const filtered = files.filter(file => !file.includes('openapi.json'));
-    return filtered.length > 0 ? `prettier --write ${filtered.join(' ')}` : [];
+    const filtered = files.filter(file => !file.includes('openapi.json'))
+    return filtered.length > 0 ? `prettier --write ${filtered.join(' ')}` : []
   },
-  '*.php': [() => 'make php-cs-fixer', () => 'make test'],
+  '*.php': (files) => {
+    const nonGenFiles = files.filter(file => !file.includes('/gen/'))
+    const commands = []
+    if (nonGenFiles.length > 0) {
+      commands.push('make php-cs-fixer', 'make test')
+    }
+    return commands
+  },
   '*Controller.php': [() => 'make openapi', () => 'git add openapi.json'],
 }
