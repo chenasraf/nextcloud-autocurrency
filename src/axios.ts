e598154381d428688aa9fd1f515737314833f0a2
Chen Asraf <casraf@pm.me>
2026-01-24 11:55:42 +0200

    fix: improve error handling

--- src/axios.ts
+++ src/axios.ts
@@ -4,11 +4,18 @@ import _axios from '@nextcloud/axios'
 const baseURL = generateOcsUrl('/apps/autocurrency/api')
 export const http = _axios.create({ baseURL })
 export const ocs = _axios.create({ baseURL })
+export const webDav = _axios.create({ baseURL: '' })
 ocs.interceptors.response.use(
   (response) => {
     const ocsData = response?.data?.ocs?.data
     response.data = ocsData ?? response?.data ?? null
     return response
   },
-  (error) => Promise.reject(error),
+  (error) => {
+    const ocsData = error.response?.data?.ocs?.data
+    if (ocsData !== undefined) {
+      error.response.data = ocsData
+    }
+    return Promise.reject(error)
+  },
 )
